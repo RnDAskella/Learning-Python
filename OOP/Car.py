4737fcb419326a3b979ba55bfdfeade0a46f68c5
RnD.Askella <rnd.askella@gmail.com>
2020-12-12 01:15:58 +0300

    2nd commit

--- OOP/Car.py
+++ OOP/Car.py
@@ -3,4 +3,14 @@ class Car:
     engine: float = 3.0
 
     def __str__(self) -> str:
-        return f"This model - {self.model}"
+        return f"This model - {self.model} and It has engine - {self.engine}"
+
+
+print(getattr(Car, "engine",
+              100))
+"""Метод, который возвращает аттрибут класса без создания экземпляра.
+Если такого аттрибута нет, то возвращает 100."""
+
+Car.color = "Red"  # устанавливаем новый аттрибут
+a1 = Car()
+print(a1.color)
